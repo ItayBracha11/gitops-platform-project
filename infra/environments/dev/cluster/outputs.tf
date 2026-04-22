42f2819f5f4e84e334cb05a3d1e1da3b5417282d
ItayBracha11 <itaybracha11@gmail.com>
2026-04-22 19:49:51 +0000

    fix: fix destroy workflows

--- infra/environments/dev/cluster/outputs.tf
+++ infra/environments/dev/cluster/outputs.tf
@@ -2,6 +2,10 @@ output "cluster_name" {
   value = module.eks.cluster_name
 }
 
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
 output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
