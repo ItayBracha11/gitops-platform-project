1039627951b9929700c06c39f55d5054760bd43b
ItayBracha11 <itaybracha11@gmail.com>
2026-04-26 14:49:34 +0000

    fix: fix backiend files

--- infra/environments/prod/network/backend.tf
+++ infra/environments/prod/network/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "itay-project-terraform-state"
     key            = "prod/network/terraform.tfstate"
-    region         = "eu-west-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
